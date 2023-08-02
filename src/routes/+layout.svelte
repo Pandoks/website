fe58c35bd5592e68016f59b70b713870825a01ed
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-08-02 15:37:56 -0700

    Added Inter font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
   import "../app.css";
 </script>
 
-<nav>
-  <a href="/">Jason Kwok</a>
-  <a href="/socials">Socials</a>
-  <a href="/journal">Journal</a>
-  <a href="/essays">Essays</a>
+<nav class="fixed p-6 w-64 h-screen">
+  <div class="flex flex-col space-y-4 text-right">
+    <a href="/">Jason Kwok</a>
+    <a href="/socials">Socials</a>
+    <a href="/journal">Journal</a>
+    <a href="/essays">Essays</a>
+  </div>
 </nav>
 
 <slot />
