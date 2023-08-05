0d8dd30b5a7f58f4afc3a5fb02e0ef1d35dacf4f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-08-05 00:19:57 -0700

    Added link structure for socials

--- src/routes/socials/+page.svelte
+++ src/routes/socials/+page.svelte
@@ -4,9 +4,41 @@
   ];
 </script>
 
-<div class="text-xs text-[#2b2b2b] ml-3 sm:ml-5 md:ml-7 lg:ml-9 xl:ml-10">
+<div class="text-[#2b2b2b] ml-3 sm:ml-5 md:ml-7 lg:ml-9 xl:ml-10">
   {#each socials as social}
-    <a href={social.href} target="_blank">{social.text}</a>
+    <a href={social.href} target="_blank">
+      <div class="flex transition hover:scale-110">
+        <span>{social.text}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 256"
+          focusable="false"
+          class="block ml-1 w-6 h-6 text-gray-500"
+        >
+          <g class="text-gray-500">
+            <line
+              x1="64"
+              y1="192"
+              x2="192"
+              y2="64"
+              fill="none"
+              stroke="rgb(143, 143, 143)"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <polyline
+              points="88 64 192 64 192 168"
+              fill="none"
+              stroke="rgb(143, 143, 143)"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></polyline>
+          </g>
+        </svg>
+      </div>
+    </a>
   {/each}
 </div>
 
