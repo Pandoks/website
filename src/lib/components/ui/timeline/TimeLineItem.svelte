08d2879bcb12b4f3e0aec85cfff163a6040a0faf
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-08-09 14:08:46 -0700

    Made entire website SSG

--- src/lib/components/ui/timeline/TimeLineItem.svelte
+++
@@ -1,3 +0,0 @@
-<script lang="ts">
-  import { selectedTime } from "./time";
-</script>
--- src/lib/components/ui/timeline/time.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-
-// Store the ID of the currently opened time
-export const selectedTime = writable(null);
