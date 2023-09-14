d99f5c7e8dbe79aeb8bfd69dd8916976df330d38
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-09-14 10:18:33 -0400

    Made svg icon

--- src/routes/socials/+page.svelte
+++ src/routes/socials/+page.svelte
@@ -8,36 +8,11 @@
   {#each socials as social}
     <a href={social.href} target="_blank" class="flex scale-group">
       <span>{social.text}</span>
-      <div class="transition hover:scale-110">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 256 256"
-          focusable="false"
-          class="block pl-1 w-6 h-6 text-gray-500 scale-target"
-        >
-          <g class="text-gray-500" transform="scale(0.8) translate(32 32)">
-            <line
-              x1="64"
-              y1="192"
-              x2="192"
-              y2="64"
-              fill="none"
-              stroke="rgb(143, 143, 143)"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="24"
-            ></line>
-            <polyline
-              points="88 64 192 64 192 168"
-              fill="none"
-              stroke="rgb(143, 143, 143)"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="24"
-            ></polyline>
-          </g>
-        </svg>
-      </div>
+      <img
+        src="/icons/link-icon.svg"
+        alt="test"
+        class="transition block pl-1 w-6 h-6 scale-target scale-90"
+      />
     </a>
   {/each}
 </div>
@@ -57,6 +32,6 @@
 
 <style>
   .scale-group:hover .scale-target {
-    transform: scale(1.5);
+    transform: scale(1.11);
   }
 </style>
