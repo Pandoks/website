1248c63f8374c180fbe10364334c2a32bfdd029e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-11-19 12:47:46 -0500

    Added projects directory

--- src/routes/socials/+page.svelte
+++ src/routes/socials/+page.svelte
@@ -4,7 +4,7 @@
   const socials = [
     { text: "Instagram", href: "https://www.instagram.com/pandoks_/" },
     { text: "GitHub", href: "https://www.github.com/pandoks/" },
-    { text: "X", href: "https://www.x.com/pandoks_/" },
+    // { text: "X", href: "https://www.x.com/pandoks_/" },
   ];
 </script>
 
