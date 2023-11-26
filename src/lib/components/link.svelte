b198e91a1e7bddbeddd2696a54012432f49e5cfd
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-11-26 13:17:13 -0500

    Removed commented link icon

--- src/lib/components/link.svelte
+++ src/lib/components/link.svelte
@@ -11,11 +11,6 @@
     class="transition block pr-1 w-6 h-6 scale-target scale-90"
   />
   <span class="text-black hover:text-gray-500 pl-1">{link.text}</span>
-  <!-- <img -->
-  <!--   src="/icons/link-icon.svg" -->
-  <!--   alt="test" -->
-  <!--   class="transition block pl-1 w-6 h-6 scale-target scale-90" -->
-  <!-- /> -->
 </a>
 
 <style>
