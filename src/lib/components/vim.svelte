99c4631db514ea9a8ea7cbe1053ccb96c8df0719
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-12-08 11:08:16 -0500

    Added store state file

--- src/lib/components/vim.svelte
+++ src/lib/components/vim.svelte
@@ -1,2 +1,32 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
+  export let active: boolean = false;
+  export let left = null;
+  export let down = null;
+  export let up = null;
+  export let right = null;
+
+  const handleKey = (event: KeyboardEvent) => {
+    const key = event.key;
+
+    switch (key) {
+      case "h":
+        break;
+      case "j":
+        break;
+      case "k":
+        break;
+      case "l":
+        break;
+      default:
+        break;
+    }
+  };
+
+  onMount(() => {
+    window.addEventListener("keydown", handleKey);
+  });
 </script>
+
+<slot />
