d531235e19d9748d9718c8f4c417eb067e9a6910
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-04-25 22:11:17 -0400

    Added motions to layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,23 @@
   import Content from "$lib/components/layout/content.svelte";
   import { onMount } from "svelte";
   import { addYank } from "$lib/components/vim/yank";
+  import Motions from "$lib/components/vim/motions.svelte";
 
   onMount(() => {
     addYank();
   });
 </script>
 
-<div
-  class="mr-auto ml-auto mt-[27vh] w-[85vw] sm:w-[75vw] md:w-[65vw] lg:ml-[27vw] lg:w-[50vw] xl:w-[40vw]"
->
-  <Navigation />
+<Motions
+  ><div
+    class="mr-auto ml-auto mt-[27vh] w-[85vw] sm:w-[75vw] md:w-[65vw] lg:ml-[27vw] lg:w-[50vw] xl:w-[40vw]"
+  >
+    <Navigation />
 
-  <div class="pl-[89.88px]">
-    <Content>
-      <slot />
-    </Content>
+    <div class="pl-[89.88px]">
+      <Content>
+        <slot />
+      </Content>
+    </div>
   </div>
-</div>
+</Motions>
