18974b9e47ddfb118e74e1932f397076559974dd
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-05-18 23:32:06 -0400

    Added easter egg upcoming store

---
+++ src/routes/store/+page.svelte
@@ -0,0 +1,16 @@
+<div class="text-xs text-[#2b2b2b]">
+  <p>Coming soon...</p>
+</div>
+
+<svelte:head>
+  <title>Jason Kwok's Store</title>
+  <meta
+    name="description"
+    content="Soon to be released store for Jason Kwok (Pandoks_)'s products"
+  />
+  <meta property="og:title" content="Jason Kwok (Pandoks_)'s upcoming store" />
+  <meta
+    property="og:description"
+    content="Soon to be released store for Jason Kwok (Pandoks_)'s products"
+  />
+</svelte:head>
